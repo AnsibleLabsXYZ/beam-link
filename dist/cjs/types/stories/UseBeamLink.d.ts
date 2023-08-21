4820669f4830d823624ba9bba7ec64e1b9d2c58e
Andrew Anderson <Anderson7301@icloud.com>
2023-08-21 17:51:31 -0500

    removed dist

--- dist/cjs/types/stories/UseBeamLink.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export interface UseBeamLinkProps {
-    linkToken: string;
-}
-/**
- * Primary UI component for user interaction
- */
-export declare const UseBeamLink: ({ linkToken }: UseBeamLinkProps) => React.JSX.Element;
